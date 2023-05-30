443f9fdb97ab17ec69fbe5160444af89291bb28c
Miroslav <miroslav@Miroslavs-MacBook-Pro.local>
2023-05-31 01:52:26 +0300

    examples from webinar2

---
+++ webinar2/index.js
@@ -0,0 +1,142 @@
+'use strict'
+/**
+ * JSDoc Basic example
+ * @author Miroslav
+ */
+
+// let userId;
+//
+// userId = 10;
+//
+// const veryBigNumber = 9783983270897402740832749328743982742398749823n;
+//
+// const API_KEY = 'dskhdbfcklsdjncksjbc';
+//
+// const isActive = true;
+//
+// const name = 'Alex';
+// console.log(`Hello ${name}`)
+
+// let userAge = true;
+
+// console.log(typeof userAge);
+
+// const data = 0;
+
+// console.log(!!data);
+
+/**
+ * Variable declaration
+ */
+
+// let id = 1;
+//
+// id = 2;
+//
+// console.log(id); // 2
+
+// const country = 'USA';
+
+// country = 'Somaly'; // will be error
+
+// console.log(country);
+
+/**
+ * Цикли
+ */
+
+// let counter = 0;
+
+/**
+ * While
+ */
+
+// while (counter < 10) {
+//     console.log('counter is ', counter);
+//     counter++;
+// }
+
+/**
+ * Do...While
+ */
+
+// do {
+//     console.log('counter is ', counter);
+//     counter++;
+// } while (counter === 10);
+
+/**
+ * For...
+ */
+
+// for (let innerCounter = 0; innerCounter < 10; innerCounter++) {
+//     console.log(counter);
+//     counter++;
+// }
+
+/**
+ * For... with break
+ */
+
+// for (let innerCounter = 0; innerCounter < 100; innerCounter++) {
+//     if (innerCounter > 15) break;
+//     console.log(innerCounter);
+// }
+
+/**
+ * For... with continue
+ */
+
+// for (let innerCounter = 0; innerCounter < 100; innerCounter++) {
+//     if (innerCounter % 2) continue;
+//     console.log(innerCounter);
+// }
+
+/**
+ * If... else
+ */
+
+// const name = undefined;
+// const age = 30;
+
+// if (name && age <= 30) {
+//     console.log('First case');
+// } else if (!name && age <= 30) {
+//     console.log('Second case');
+// } else {
+//     console.log('Third case');
+// }
+
+/**
+ * Тернарний оператор
+ */
+
+// const name = 'Alex';
+// const age = 30;
+// const isOnline = false;
+// Нижче приведений хороший приклад
+// let isUserActive = name && isOnline ? 'is online' : 'is offline';
+//
+// console.log(isUserActive);
+
+// Нижче приведений антипатерн
+// const userCase = name && age <= 30 ? 'First case' : !name && age <= 30 ? 'Second case' : 'Third case';
+
+// console.log(userCase);
+
+/**
+ * Switch
+ */
+
+// const userName = 'Alex';
+//
+// switch (userName) {
+//     case 'Alex':
+//         console.log('Hello Alex');
+//         break;
+//     case 'Max':
+//         console.log('Hello Max');
+//         break;
+//     default:
+//         console.log('Hello stranger');
+// }
